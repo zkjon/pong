f348a5c0b3faf2fa8f33457b1cc728f14f294615
zkjon <121103151+zkj0n@users.noreply.github.com>
2025-09-04 08:34:55 +0200

    feat: configure Astro with Preact and Tailwind CSS integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+import tailwindcss from '@tailwindcss/vite';
+
+import path from 'path';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact()],
+
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
+  devToolbar: {
+    enabled: false,
+  },
+});
